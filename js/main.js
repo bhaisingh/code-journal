b984accf35f08c9dae9392b56462b1aeeb699ab5
Rakesh Singh <bhaisingh@gmail.com>
2021-05-23 11:43:42 -0700

    Created dummy unordered list to conifrm the display

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const $notes = document.querySelector('#notes');
 const $imgSrc = document.querySelector('.image-entry');
 const $entriesDisplay = document.querySelector('[data-view="entries"]');
 const $entriesAdd = document.querySelector('[data-view="entry-form"]');
+const $unorderEntry = document.querySelector('.entry-Unordered-list');
 
 $chPhotoURL.addEventListener('input', function (e) {
   $imgSrc.setAttribute('src', $chPhotoURL.value);
@@ -30,18 +31,42 @@ const $entryPage = document.querySelector('.entry-page');
 const $buttonNew = document.querySelector('.button-new');
 document.addEventListener('click', function (e) {
   if (e.target === $entryPage) {
-    renderEntryPage();
+    renderEntriesPage();
   } else if (e.target === $buttonNew) {
     renderNewEntryPage();
   }
 });
 
-function renderEntryPage() {
+function renderEntriesPage() {
   $entriesDisplay.className = 'entries';
   $entriesAdd.className = 'create-entry hidden';
+  if (data.nextEntryId > 1) {
+    for (let i = 0; i < data.length; i++) {
+      buildEntriesDomObject(data[i]);
+    }
+  }
 }
 
 function renderNewEntryPage() {
   $entriesDisplay.className = 'entries hidden';
   $entriesAdd.className = 'create-entry';
+}
+
+function buildEntriesDomObject(entryObject) {
+  const listItem = document.createElement('li');
+  $unorderEntry.appendChild(listItem);
+
+  const row = document.createElement('div');
+  row.setAttribute('class', 'row');
+  $unorderEntry.appendChild(row);
+
+  const colHalf = document.createElement('div');
+  colHalf.setAttribute('class', 'column-half');
+  row.appendChild(colHalf);
+
+  const image = document.createElement('img');
+  image.setAttribute('src', entryObject.PhotoURL);
+  image.setAttribute('class', 'image');
+  colHalf.appendChild(image);
+
 }
